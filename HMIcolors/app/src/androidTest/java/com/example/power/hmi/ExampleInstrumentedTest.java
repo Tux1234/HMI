04067219daace0053da443c054fbaf30dc8c627e
Zapzarapza <marko.oreskovic1995@gmail.com>
2017-12-28 13:43:23 +0100

    Add files via upload

--- HMIcolors/app/src/androidTest/java/com/example/power/hmi/ExampleInstrumentedTest.java
+++ HMIcolors/app/src/androidTest/java/com/example/power/hmi/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.power.hmi;
+package com.example.power.hmicolors;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.power.hmi", appContext.getPackageName());
+        assertEquals("com.example.power.hmicolors", appContext.getPackageName());
     }
 }
